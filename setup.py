0fede9cdd6f4e03009001f44e3c7ac033d1bd00c
Hugo <humongo.shi@gmail.com>
2013-02-09 12:13:48 -0500

    Added setup.py, and hemlib

---
+++ setup.py
@@ -0,0 +1,20 @@
+# needs to be tested
+import sys
+if len(sys.argv)>1 and sys.argv[1] == 'develop':
+    # Only import setuptools if we have to
+    import setuptools
+from distutils.core import setup
+import os
+import sys
+__version__ = (0, 0, 1)
+
+setup(
+    name = 'continuumweb',
+    version = '.'.join([str(x) for x in __version__]),
+    packages = ['continuumweb']
+    author = 'Continuum Analytics',
+    author_email = 'info@continuum.io',
+    url = 'http://github.com/ContinuumIO/continuumweb',
+    zip_safe=False,
+    license = 'New BSD',
+)
