13b7ff5efafec305570cf16face1f68f3b881d44
Hugo <humongo.shi@gmail.com>
2013-02-12 09:35:21 -0500

    slug_path is now configureable changes to support django

--- continuumweb/hemlib.py
+++ continuumweb/hemlib.py
@@ -1,18 +1,28 @@
-import flask
 import json
 import os
 import re
 import urlparse
 import sys
 
+slug_path = None
+
 def slug_json():
-    path = os.path.join(slug_path(), 'slug.json')
+    path = os.path.join(slug_path, 'slug.json')
     with open(path) as f:
         return json.load(f)
-    
-def slug_path():
     return os.path.join(os.path.dirname(__file__))
 
+def hemprefixes():
+    prefixes = [os.path.join(slug_path, x) for x in slug_json()['paths']]
+    prefixes = [os.path.normpath(x) for x in prefixes]
+    return prefixes
+
+def all_coffee_assets(host, port):
+    targets = []
+    for prefix in hemprefixes():
+        targets.extend(coffee_assets(prefix, host, port))
+    return targets
+                       
 ignores = [".*~", "^#", "^\.#"]
 def coffee_assets(prefix, host, port):
     #walk coffee tree
@@ -26,8 +36,8 @@ def coffee_assets(prefix, host, port):
     ftargets = [f for f in ftargets if not \
              any([re.match(ignore, os.path.basename(f)) for ignore in ignores])]
     #make path relative to slugdir
-    slugpath = slug_path()
-    ftargets = [os.path.relpath(x, slug_path()) for x in ftargets]
+    slugpath = slug_path
+    ftargets = [os.path.relpath(x, slug_path) for x in ftargets]
     
     #remove extension
     ftargets = [os.path.splitext(f)[0] for f in ftargets]
@@ -36,7 +46,14 @@ def coffee_assets(prefix, host, port):
     return [urlparse.urljoin(base, x) for x in ftargets]
     
 def slug_libs(app, libs):
-    targets = [os.path.join(slug_path(), os.path.normpath(x)) for x in libs]
+    import flask
+    targets = [os.path.join(slug_path, os.path.normpath(x)) for x in libs]
     targets = [os.path.relpath(x, app.static_folder) for x in targets]
     targets = [flask.url_for('static', filename=x) for x in targets]
+    return targets
+
+def django_slug_libs(static_root, static_url, libs):
+    targets = [os.path.join(slug_path, os.path.normpath(x)) for x in libs]
+    targets = [os.path.relpath(x, static_root) for x in targets]
+    targets = [urlparse.urljoin(static_url, x) for x in targets]
     return targets
